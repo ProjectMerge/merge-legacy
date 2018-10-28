518b0807a2895f44b9d3bb315a5d5c98a8fe3470
barrystyle <barrystyle@westnet.com.au>
2018-10-28 12:43:06 +0800

    RPC referencing PIVX corrected

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -238,10 +238,10 @@ Value stop(const Array& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop PIVX server.");
+            "\nStop MERGE server.");
     // Shutdown will take long enough that the response should get back
     StartShutdown();
-    return "PIVX server stopping";
+    return "MERGE server stopping";
 }
 
 
