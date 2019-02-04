02e1b0854b13c6a4aec6d5c069c0f0b35dc9ad1c
coin-server <csadmin@coin-server.com>
2019-02-04 13:29:25 -0500

    [Error] added missing || in clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -10,9 +10,8 @@
 #endif //HAVE_CONFIG_H
 
 // Check that required client information is defined
-#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
-!defined(COPYRIGHT_YEAR)
-#error Client version information missing: version is not defined by merge-config.h or in any other way
+#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR) || !defined(COPYRIGHT_YEAR)
+//#error Client version information missing: version is not defined by merge-config.h or in any other way
 #endif
 
 /**
