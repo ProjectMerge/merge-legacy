9a2ddc2b53e74f7509253c1b489171e7b388ff10
Sascha <info@aviator-coding.de>
2019-05-24 23:05:48 -0700

    [RPC] - modified getrawtransaction

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -75,7 +75,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
         {"getblockheader", 1},
         {"gettransaction", 1},
         {"getrawtransaction", 1},
-        {"getrawtransaction", 1},
+        {"getrawtransaction", 2},
         {"createrawtransaction", 0},
         {"createrawtransaction", 1},
         {"signrawtransaction", 1},
